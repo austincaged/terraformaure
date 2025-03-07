661c021cc2ccc238206b31448191972d1739e2e0
Ubuntu <onoadmin@ono-web-vm.chlnt1nkojoedppeobcziolvyg.ax.internal.cloudapp.net>
2025-03-07 18:49:56 +0000

    work6

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 header('Content-Type: text/html');
-echo "<html><body><h1>This iiiiis desfss my website. it worked finallllyyyyyyyyyy!!!!!!!!!!!!!!</h1></body></html>";
+echo "<html><body><h1>Welcome to  my website. it worked finallllyyyyyyyyyy!!!!!!!!!!!!!!</h1></body></html>";
 ?>
