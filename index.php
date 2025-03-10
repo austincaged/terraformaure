9a0de0de215db892494d8019c750e7e9471e040a
Ubuntu <onoadmin@ono-web-vm.chlnt1nkojoedppeobcziolvyg.ax.internal.cloudapp.net>
2025-03-10 09:24:52 +0000

    work 16

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 header('Content-Type: text/html');
-echo "<html><body><h1>Welcome to built  website. WE DID IT. it worked finallllyyyyyyyyyy!!!!!!!!!!!!!!</h1></body></html>";
+echo "<html><body><h1>Live Demo</h1></body></html>";
 ?>
